18539daa0562ed59e4b8577aad857dad16122884
Enric Cervera <ecervera@uji.es>
2024-07-18 16:05:11 +0200

    Use host network

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rocker --name point_lio --x11 --nvidia --volume ./bags:/bags -- centauro/point_lio_unilidar:latest bash
+rocker --name point_lio --x11 --nvidia --network host --volume ./bags:/bags -- centauro/point_lio_unilidar:latest bash
